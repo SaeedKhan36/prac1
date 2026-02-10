e3ece4b7f818c74d1702454b7ecdff1a5e6c9e92
SaeedKhan36 <saeedkhan364.com@gmail.com>
2026-02-10 15:07:08 +0530

    revising class concepts

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-class Rectangle {
+class Rectangle extends shape {
     constructor (height,length,color){
+        super(color);
         this.height= height;
         this.length= length;
-        this.color= color;
+   
 
     }
 
@@ -21,3 +22,54 @@ class Rectangle {
   const r2 = new Rectangle (2,4,"black");
   console.log(r2.area())
   console.log(r2.perimeter())
+
+
+
+class circle extends shape {
+    constructor (radius,color){
+        super(color);   
+        this.radius= radius;
+       
+
+    }
+
+    area(){
+        return Math.PI * this.radius * this.radius
+    }
+
+    circumference(){
+        return 2 * Math.PI * this.radius
+    }
+
+    paint(){
+        return `The color of the circle is ${this.color}`
+    }
+} 
+
+class square extends shape {
+    constructor (side,color){
+        super(color);
+        this.side= side;
+    }
+
+    area(){
+        return this.side * this.side
+    }
+
+    perimeter(){
+        return 4 * this.side
+    }
+    paint(){
+        return `The color of the square is ${this.color}`
+    }
+}
+
+class shape {
+    constructor (color){
+        this.color= color;
+
+    }
+    paint(){
+        return `The color of the shape is ${this.color}`
+    }
+}
